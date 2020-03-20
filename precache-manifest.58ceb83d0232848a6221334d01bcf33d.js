e24c7d9353506a2d3501de87521ea9131bfec5b0
tun4tun <bod2na@gmail.com>
2020-03-20 22:07:37 +0300

    Updates

---
+++ precache-manifest.58ceb83d0232848a6221334d01bcf33d.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0abfeb38a40a20f170bce1418dffa577",
+    "url": "/lastikstok/index.html"
+  },
+  {
+    "revision": "0689b7f1d40175e50384",
+    "url": "/lastikstok/static/css/main.ad4b8ce5.chunk.css"
+  },
+  {
+    "revision": "3ebcabf0fe89462c0c21",
+    "url": "/lastikstok/static/js/2.f2978859.chunk.js"
+  },
+  {
+    "revision": "cd140b39a68572614de75b4ff55c2994",
+    "url": "/lastikstok/static/js/2.f2978859.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0689b7f1d40175e50384",
+    "url": "/lastikstok/static/js/main.45f1c75b.chunk.js"
+  },
+  {
+    "revision": "a000a413561572de9d8c1e3fcc6eccf8",
+    "url": "/lastikstok/static/js/main.45f1c75b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba2d54f37df371c20c3a",
+    "url": "/lastikstok/static/js/runtime-main.77185bd9.js"
+  },
+  {
+    "revision": "275a2629582aa2af3469364c6748cf38",
+    "url": "/lastikstok/static/media/argon-react-white.275a2629.png"
+  },
+  {
+    "revision": "f38ddea9dedadea03c5d43c596dac13f",
+    "url": "/lastikstok/static/media/argon-react.f38ddea9.png"
+  },
+  {
+    "revision": "48461ea4e797c9774dabb4a0440d2f56",
+    "url": "/lastikstok/static/media/fa-brands-400.48461ea4.woff2"
+  },
+  {
+    "revision": "7b464e274bc331f9a765d765359635a5",
+    "url": "/lastikstok/static/media/fa-brands-400.7b464e27.woff"
+  },
+  {
+    "revision": "947b9537bc0fecc8130d48eb753495a1",
+    "url": "/lastikstok/static/media/fa-brands-400.947b9537.ttf"
+  },
+  {
+    "revision": "9b6c8da3c489424e2b3e9c9fb6314b37",
+    "url": "/lastikstok/static/media/fa-brands-400.9b6c8da3.eot"
+  },
+  {
+    "revision": "b5472631dbace30d549357ec325b9c62",
+    "url": "/lastikstok/static/media/fa-brands-400.b5472631.svg"
+  },
+  {
+    "revision": "381af09a1366b6c2ae65eac5dd6f0588",
+    "url": "/lastikstok/static/media/fa-regular-400.381af09a.woff"
+  },
+  {
+    "revision": "73fe7f1effbf382f499831a0a9f18626",
+    "url": "/lastikstok/static/media/fa-regular-400.73fe7f1e.ttf"
+  },
+  {
+    "revision": "7422060ca379ee9939d3b687d072acad",
+    "url": "/lastikstok/static/media/fa-regular-400.7422060c.eot"
+  },
+  {
+    "revision": "949a2b066ec37f5a384712fc7beaf2f1",
+    "url": "/lastikstok/static/media/fa-regular-400.949a2b06.woff2"
+  },
+  {
+    "revision": "b5a61b229c9c92a6ac21f5b0e3c6e9f1",
+    "url": "/lastikstok/static/media/fa-regular-400.b5a61b22.svg"
+  },
+  {
+    "revision": "0079a0ab6bec4da7d6e16f2a2e87cd71",
+    "url": "/lastikstok/static/media/fa-solid-900.0079a0ab.ttf"
+  },
+  {
+    "revision": "14a08198ec7d1eb96d515362293fed36",
+    "url": "/lastikstok/static/media/fa-solid-900.14a08198.woff2"
+  },
+  {
+    "revision": "38508b2e7fac045869a86a15936433f7",
+    "url": "/lastikstok/static/media/fa-solid-900.38508b2e.svg"
+  },
+  {
+    "revision": "70e65a7d34902f2c350816ecfe2f6492",
+    "url": "/lastikstok/static/media/fa-solid-900.70e65a7d.eot"
+  },
+  {
+    "revision": "815694de1120d6c1e9d1f0895ee81056",
+    "url": "/lastikstok/static/media/fa-solid-900.815694de.woff"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/lastikstok/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/lastikstok/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/lastikstok/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/lastikstok/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/lastikstok/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "e2582654b8e7b6c285d5017df6427d73",
+    "url": "/lastikstok/static/media/profile-cover.e2582654.jpg"
+  },
+  {
+    "revision": "e7c82e5c569db9d9761b13ef1384f702",
+    "url": "/lastikstok/static/media/sketch.e7c82e5c.jpg"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/lastikstok/static/media/team-1-800x800.53033970.jpg"
+  }
+]);
